d82b11044f198fe0fd4abae07678eff31976a6b0
kachi <971759126@qq.com>
2023-01-08 19:43:35 +0800

    修改debug以对接axi

--- mycpu_top.v
+++ mycpu_top.v
@@ -146,111 +146,24 @@ mips mips(
     .debug_wb_rf_wnum  (debug_wb_rf_wnum  ),
     .debug_wb_rf_wdata (debug_wb_rf_wdata )
 );
-bridge_1x2 bridge_1x2 (
-    .no_dcache(no_dcache),
-    //mips传出的data请求
-    .cpu_data_req     (cpu_data_req  ),     .cpu_data_wr      (cpu_data_wr   ),
-    .cpu_data_addr    (cpu_data_addr ),     .cpu_data_wdata   (cpu_data_wdata),
-    .cpu_data_size    (cpu_data_size ), 
-    //传向mips的data回复
-    .cpu_data_rdata   (cpu_data_rdata),
-    .cpu_data_addr_ok (cpu_data_addr_ok),   .cpu_data_data_ok (cpu_data_data_ok),
-
-    //应当前往cache的data请求
-    .ram_data_req     (ram_data_req  ),     .ram_data_wr      (ram_data_wr   ),
-    .ram_data_addr    (ram_data_addr ),     .ram_data_wdata   (ram_data_wdata),
-    .ram_data_size    (ram_data_size ), 
-    //从cache得到的data回复
-    .ram_data_rdata   (ram_data_rdata),
-    .ram_data_addr_ok (ram_data_addr_ok),   .ram_data_data_ok (ram_data_data_ok),
-
-    //不过cache的data请求
-    .conf_data_req     (conf_data_req  ),     .conf_data_wr      (conf_data_wr   ),
-    .conf_data_addr    (conf_data_addr ),     .conf_data_wdata   (conf_data_wdata),
-    .conf_data_size    (conf_data_size ), 
-    //不过cache得到的data回复
-    .conf_data_rdata   (conf_data_rdata),
-    .conf_data_addr_ok (conf_data_addr_ok),   .conf_data_data_ok (conf_data_data_ok)
-);
-
-d_cache d_cache (
-    .clk(clk), .rst(rst),
-
-    .cpu_data_req(ram_data_req),    .cpu_data_wr(ram_data_wr),
-    .cpu_data_size(ram_data_size),  .cpu_data_addr(ram_data_addr),
-    .cpu_data_wdata(ram_data_wdata), 
-
-    .cpu_data_rdata(ram_data_rdata),
-    .cpu_data_addr_ok(ram_data_addr_ok) ,.cpu_data_data_ok(ram_data_data_ok) ,
-
-    .cache_data_req(cache_data_req),    .cache_data_wr(cache_data_wr),
-    .cache_data_size(cache_data_size),  .cache_data_addr(cache_data_addr),
-    .cache_data_wdata(cache_data_wdata), 
-
-    .cache_data_rdata(cache_data_rdata),
-    .cache_data_addr_ok(cache_data_addr_ok) ,.cache_data_data_ok(cache_data_data_ok)
-);
-
-i_cache i_cache(
-    .clk(clk), .rst(rst),
-    
-    .cpu_inst_req(cpu_inst_req),    .cpu_inst_wr(cpu_inst_wr),
-    .cpu_inst_size(cpu_inst_size),  .cpu_inst_addr(cpu_inst_addr),
-    .cpu_inst_wdata(cpu_inst_wdata), 
-
-    .cpu_inst_rdata(cpu_inst_rdata),
-    .cpu_inst_addr_ok(cpu_inst_addr_ok) ,.cpu_inst_data_ok(cpu_inst_data_ok) ,
-
-    .cache_inst_req(cache_inst_req),    .cache_inst_wr(cache_inst_wr),
-    .cache_inst_size(cache_inst_size),  .cache_inst_addr(cache_inst_addr),
-    .cache_inst_wdata(cache_inst_wdata), 
-
-    .cache_inst_rdata(cache_inst_rdata),
-    .cache_inst_addr_ok(cache_inst_addr_ok) ,.cache_inst_data_ok(cache_inst_data_ok)
-);
-bridge_2x1 bridge_2x1(
-    .no_dcache(no_dcache),
-    //应当前往cache的data请求
-    .ram_data_req     (ram_data_req  ),     .ram_data_wr      (ram_data_wr   ),
-    .ram_data_addr    (ram_data_addr ),     .ram_data_wdata   (ram_data_wdata),
-    .ram_data_size    (ram_data_size ), 
-    //从cache得到的data回复
-    .ram_data_rdata   (ram_data_rdata),
-    .ram_data_addr_ok (ram_data_addr_ok),   .ram_data_data_ok (ram_data_data_ok),
-
-    //不过cache的data请求
-    .conf_data_req     (conf_data_req  ),     .conf_data_wr      (conf_data_wr   ),
-    .conf_data_addr    (conf_data_addr ),     .conf_data_wdata   (conf_data_wdata),
-    .conf_data_size    (conf_data_size ), 
-    //不过cache得到的data回复
-    .conf_data_rdata   (conf_data_rdata),
-    .conf_data_addr_ok (conf_data_addr_ok),   .conf_data_data_ok (conf_data_data_ok),
-    //不过cache的data请求
-    .wrap_data_req     (wrap_data_req  ),     .wrap_data_wr      (wrap_data_wr   ),
-    .wrap_data_addr    (wrap_data_addr ),     .wrap_data_wdata   (wrap_data_wdata),
-    .wrap_data_size    (wrap_data_size ), 
-    //不过cache得到的data回复
-    .wrap_data_rdata   (wrap_data_rdata),
-    .wrap_data_addr_ok (wrap_data_addr_ok),   .wrap_data_data_ok (wrap_data_data_ok)
-);
 
 cpu_axi_interface axi_interface(
     .clk(clk), .resetn(aresetn),
-    //input
-    .inst_req(cpu_inst_req),    .inst_wr(cpu_inst_wr),
-    .inst_size(cpu_inst_size),  .inst_addr(cpu_inst_addr),
-    .inst_wdata(cpu_inst_wdata), 
-    //output
-    .inst_rdata(cpu_inst_rdata),
-    .inst_addr_ok(cpu_inst_addr_ok) ,.inst_data_ok(cpu_inst_data_ok) ,
+    //mips传出的inst请求
+    .inst_req     (cpu_inst_req  ),     .inst_wr      (cpu_inst_wr   ),
+    .inst_addr    (cpu_inst_addr ),     .inst_size    (cpu_inst_size ),
+    .inst_wdata   (cpu_inst_wdata),     
+    //mips得到的inst回复
+    .inst_rdata   (cpu_inst_rdata),
+    .inst_addr_ok (cpu_inst_addr_ok),   .inst_data_ok (cpu_inst_data_ok),
 
     //input
-    .data_req     (wrap_data_req  ),     .data_wr      (wrap_data_wr   ),
-    .data_addr    (wrap_data_addr ),     .data_wdata   (wrap_data_wdata),
-    .data_size    (wrap_data_size ), 
+    .data_req     (cpu_data_req  ),     .data_wr      (cpu_data_wr   ),
+    .data_addr    (cpu_data_addr ),     .data_wdata   (cpu_data_wdata),
+    .data_size    (cpu_data_size ), 
     //output
-    .data_rdata   (wrap_data_rdata),
-    .data_addr_ok (wrap_data_addr_ok),   .data_data_ok (wrap_data_data_ok),
+    .data_rdata   (cpu_data_rdata),
+    .data_addr_ok (cpu_data_addr_ok),   .data_data_ok (cpu_data_data_ok),
 
     //input
     .arid(arid),            .araddr(araddr),   .arlen(arlen),
