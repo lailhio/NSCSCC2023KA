633a986996235920cb6e40b73b55be395c781f7e
kachi <971759126@qq.com>
2023-01-12 01:49:16 +0800

    修改d_cache二路组

--- d_cache.v
+++ d_cache.v
@@ -1,24 +1,24 @@
 module d_cache (
     input wire clk, rst,
     //mips core
-    input         cpu_data_req     ,
-    input         cpu_data_wr      ,
-    input  [1 :0] cpu_data_size    ,
+    input         cpu_data_req     , // 是不是数据请求(load 或 store指令)。一个周期后就清除了
+    input         cpu_data_wr      , // mips->cache, 当前请求是否是写请求(是不是store指令)。一直保持电平状态直到请求成功
+    input  [1 :0] cpu_data_size    , // 结合地址最低两位，确定数据的有效字节（用于sb、sh等指令）
     input  [31:0] cpu_data_addr    ,
     input  [31:0] cpu_data_wdata   ,
     output [31:0] cpu_data_rdata   ,
-    output        cpu_data_addr_ok ,
-    output        cpu_data_data_ok ,
+    output        cpu_data_addr_ok , // 确认请求的地址已收到，一个周期后清除
+    output        cpu_data_data_ok , // 确认请求的数据已收到，一个周期后清除
 
     //axi interface
-    output         cache_data_req     ,
-    output         cache_data_wr      ,
+    output         cache_data_req     , // cache->mem, 是不是数据请求(RM或WM状态)。一个周期后就清除了
+    output         cache_data_wr      , // cache->mem, 当前请求是否是写请求(是不是处于WM状态)。一直保持电平状态直到请求成功
     output  [1 :0] cache_data_size    ,
     output  [31:0] cache_data_addr    ,
     output  [31:0] cache_data_wdata   ,
     input   [31:0] cache_data_rdata   ,
-    input          cache_data_addr_ok ,
-    input          cache_data_data_ok 
+    input          cache_data_addr_ok , // 确认请求的地址已收到，一个周期后清除
+    input          cache_data_data_ok   // 确认请求的数据已收到，一个周期后清除
 );
     //Cache配置
     parameter  INDEX_WIDTH  = 10, OFFSET_WIDTH = 2;
@@ -26,6 +26,14 @@ module d_cache (
     localparam CACHE_DEEPTH = 1 << INDEX_WIDTH;
     localparam DATA_WIDTH   = 32;
     
+    //Cache存储单元
+    //* 两路，所以cache扩大一倍
+    reg [1:0]             cache_valid [CACHE_DEEPTH - 1 : 0];
+    reg [1:0]             cache_dirty [CACHE_DEEPTH - 1 : 0]; // 是否修改过
+    reg [1:0]             cache_ru    [CACHE_DEEPTH - 1 : 0]; //* recently used
+    reg [2*TAG_WIDTH-1:0] cache_tag   [CACHE_DEEPTH - 1 : 0];
+    reg [2*DATA_WIDTH-1:0]cache_block [CACHE_DEEPTH - 1 : 0];
+
     //访问地址分解
     wire [OFFSET_WIDTH-1:0] offset;
     wire [INDEX_WIDTH-1:0] index;
@@ -35,70 +43,44 @@ module d_cache (
     assign index = cpu_data_addr[INDEX_WIDTH + OFFSET_WIDTH - 1 : OFFSET_WIDTH];
     assign tag = cpu_data_addr[31 : INDEX_WIDTH + OFFSET_WIDTH];
 
-    //Cache存储单元，四路组相联，所以cache[3:0]
-    reg [3:0]               cache_valid [CACHE_DEEPTH - 1 : 0];
-    reg [3:0]               cache_dirty [CACHE_DEEPTH - 1 : 0]; // 是否被修改了，即是否脏了
-    reg [4*TAG_WIDTH-1:0]   cache_tag   [CACHE_DEEPTH - 1 : 0];
-    reg [4*DATA_WIDTH-1:0]  cache_block [CACHE_DEEPTH - 1 : 0];
-    // 伪LRU的查找树，对于 4 路的结构，每一个 cache set 需要 3bit 来存储最近使用信息。
-    reg [2:0]           tree_table  [CACHE_DEEPTH - 1 : 0];
-    // tree 为对应cache set的查找树, tree[2]为根节点, tree[1]右子树，tree[0]左子树
-    wire [2:0] tree;
-    assign tree = tree_table[index];
-
     //访问Cache line
-    wire                 c_valid[3:0];
-    wire                 c_dirty[3:0]; 
-    wire [TAG_WIDTH-1:0] c_tag  [3:0];
-    wire [31:0]          c_block[3:0];
+    wire                  c_valid[1:0];
+    wire                  c_dirty[1:0]; // 是否修改过
+    wire                  c_ru   [1:0]; //* recently used
+    wire [TAG_WIDTH-1:0]  c_tag  [1:0];
+    wire [2*DATA_WIDTH-1:0]c_block[1:0];
 
     assign c_valid[0] = cache_valid[index][0];
     assign c_valid[1] = cache_valid[index][1];
-    assign c_valid[2] = cache_valid[index][2];
-    assign c_valid[3] = cache_valid[index][3];
-
     assign c_dirty[0] = cache_dirty[index][0];
     assign c_dirty[1] = cache_dirty[index][1];
-    assign c_dirty[2] = cache_dirty[index][2];
-    assign c_dirty[3] = cache_dirty[index][3];
+    assign c_ru   [0] = cache_ru   [index][0];
+    assign c_ru   [1] = cache_ru   [index][1];
 
     assign c_tag  [0] = cache_tag  [index][1*TAG_WIDTH-1:0*TAG_WIDTH];
     assign c_tag  [1] = cache_tag  [index][2*TAG_WIDTH-1:1*TAG_WIDTH];
-    assign c_tag  [2] = cache_tag  [index][3*TAG_WIDTH-1:2*TAG_WIDTH];
-    assign c_tag  [3] = cache_tag  [index][4*TAG_WIDTH-1:3*TAG_WIDTH];
 
     assign c_block[0] = cache_block[index][1*DATA_WIDTH-1:0*DATA_WIDTH];
     assign c_block[1] = cache_block[index][2*DATA_WIDTH-1:1*DATA_WIDTH];
-    assign c_block[2] = cache_block[index][3*DATA_WIDTH-1:2*DATA_WIDTH];
-    assign c_block[3] = cache_block[index][4*DATA_WIDTH-1:3*DATA_WIDTH];
 
     //判断是否命中
     wire hit, miss;
-    assign hit = c_valid[0] & (c_tag[0] == tag) | 
-                 c_valid[1] & (c_tag[1] == tag) |
-                 c_valid[2] & (c_tag[2] == tag) |
-                 c_valid[3] & (c_tag[3] == tag);  // cache line某一路中的valid位为1，且tag与地址中tag相等
+    assign hit = c_valid[0] & (c_tag[0] == tag) | c_valid[1] & (c_tag[1] == tag);  //* cache line有一路中的valid位为1，且tag与地址中tag相等
     assign miss = ~hit;
 
-    // 后面的cache处理应访问哪一路
-    wire [1:0] c_way;
-    // 1. hit，选hit的那一路
-    // 2. miss，选伪LRU查找树索引出的最近未使用的那一路:
-    // 索引右子树: tree[2]==0 -> c_way = {tree[2], tree[1]}
-    // 索引左子树: tree[2]==1 -> c_way = {tree[2], tree[0]}  
-    assign c_way = hit ? (c_valid[0] & (c_tag[0] == tag) ? 2'b00 :
-                          c_valid[1] & (c_tag[1] == tag) ? 2'b01 :
-                          c_valid[2] & (c_tag[2] == tag) ? 2'b10 :
-                          2'b11) : 
-                   tree[2] ? {tree[2], tree[0]} : // 索引左子树
-                             {tree[2], tree[1]};  // 索引右子树
+    //* 后面的cache处理应访问哪一路
+    wire c_way;
+    //* 1. hit，选hit的那一路
+    //* 2. miss，选不是最近使用的那一路(c_ru[0]==1，0路最近使用 -> c_way=1路)
+    assign c_way = hit ? (c_valid[0] & (c_tag[0] == tag) ? 1'b0 : 1'b1) : 
+                   c_ru[0] ? 1'b1 : 1'b0; 
 
-    // read or write
-    wire read, write;
-    assign write = cpu_data_wr;
-    assign read =  ~write; 
+    // cpu请求是不是读或写请求(是不是load或store指令)
+    wire load, store;
+    assign store = cpu_data_wr;
+    assign load = cpu_data_req & ~store; // 是数据请求，且不是store，那么就是load
 
-    // cache当前位置是否dirty
+    //* cache当前位置是否dirty
     wire dirty, clean;
     assign dirty = c_dirty[c_way];
     assign clean = ~dirty;
@@ -106,7 +88,7 @@ module d_cache (
     //FSM
     parameter IDLE = 2'b00, RM = 2'b01, WM = 2'b11;
     reg [1:0] state;
-    // write指令，是否是处在RM状态（发生了miss)。当RM结束时(state从RM->IDLE)的上升沿，in_RM读出来仍为1.
+    // store指令，是否是处在RM状态（发生了miss)。当RM结束时(state从RM->IDLE)的上升沿，in_RM读出来仍为1.
     reg in_RM;
 
     always @(posedge clk) begin
@@ -143,53 +125,91 @@ module d_cache (
 
                     in_RM <= 1'b1;
                 end
-                default:begin
-                    state <= IDLE;
-                    in_RM <= 1'b0;
-                end
             endcase
         end
     end
 
     //读内存
-    //变量 read_req, addr_rcv, read_finish用于构造类sram信号。
-    wire read_req;      //一次完整的读事务，从发出读请求到结束
-    reg addr_rcv;       //地址接收成功(addr_ok)后到结束      
-    wire read_finish;   //数据接收成功(data_ok)，即读请求结束
+    //变量 isRM, addr_rcv, read_finish用于构造类sram信号。
+    wire isRM;      //一次完整的读事务，从发出读请求到结束 // 是不是处于RM状态
+    reg addr_rcv;       //地址接收成功(addr_ok)后到结束      // 处于RM状态，且地址已得到mem的确认
+    wire read_finish;   //数据接收成功(data_ok)，即读请求结束 // 处于RM状态，且已得到mem读取的数据
     always @(posedge clk) begin
         addr_rcv <= rst ? 1'b0 :
-                    cache_data_req & read_req & cache_data_addr_ok ? 1'b1 :
-                    read_finish ? 1'b0 : addr_rcv;
+                    cache_data_req & isRM & cache_data_addr_ok ? 1'b1 :
+                    read_finish ? 1'b0 : 
+                    addr_rcv;
     end
-    assign read_req = state==RM;
-    assign read_finish = read_req & cache_data_data_ok;
+    assign isRM = state==RM;
+    assign read_finish = isRM & cache_data_data_ok;
 
     //写内存
-    wire write_req;     // 是不是处于WM状态
+    wire isWM;     // 是不是处于WM状态
     reg waddr_rcv;      // 处于WM状态，且地址已得到mem的确认
     wire write_finish;  // 处于WM状态，且已写入mem的数据
     always @(posedge clk) begin
         waddr_rcv <= rst ? 1'b0 :
-                     cache_data_req& write_req & cache_data_addr_ok ? 1'b1 :
-                     write_finish ? 1'b0 : waddr_rcv;
+                     cache_data_req& isWM & cache_data_addr_ok ? 1'b1 :
+                     write_finish ? 1'b0 :
+                     waddr_rcv;
     end
-    assign write_req = state==WM;
-    assign write_finish = write_req & cache_data_data_ok;
+    assign isWM = state==WM;
+    assign write_finish = isWM & cache_data_data_ok;
 
     //output to mips core
+    //* 输出对应路的cache block
     assign cpu_data_rdata   = hit ? c_block[c_way] : cache_data_rdata;
-    assign cpu_data_addr_ok = cpu_data_req & hit | cache_data_req & read_req & cache_data_addr_ok;
-    assign cpu_data_data_ok = cpu_data_req & hit | read_req & cache_data_data_ok;
+
+    // 确认地址
+    // 如果是写指令store，命中，那么直接确认地址;
+    //                  缺失，请求mem且还未收到地址(cache_data_req)，
+    //                       干净，那么等读mem时(isRM)，mem确认完地址(cache_data_addr_ok)时确认；
+    //                       脏，那么等写完mem后，等到读mem时(isRM)，mem确认完地址(cache_data_addr_ok)时确认；
+    
+    // 如果是读指令load ，命中，那么就直接确认地址；
+    //                  缺失，请求mem且还未收到地址(cache_data_req)，
+    //                       干净，那么等读mem时(isRM)，mem确认完地址(cache_data_addr_ok)时确认；
+    //                       脏，那么等写完mem后，等到读mem时(isRM)，mem确认完地址(cache_data_addr_ok)时确认
+    // 综合一下:
+    // if (cpu_data_req & hit){
+    //     return 1'b1;
+    // }else if (cache_data_req & isRM){
+    //     return cache_data_addr_ok;
+    // }
+    // 即 -> cpu_data_req & hit | cache_data_req & isRM & cache_data_addr_ok;
+    assign cpu_data_addr_ok = cpu_data_req & hit | cache_data_req & isRM & cache_data_addr_ok;
+
+    // 确认数据
+    // 如果是写指令store，命中，那么更新cache的同时确认数据；
+    //                  缺失，干净，那么等读mem时(isRM)，mem确认完数据(cache_data_data_ok)后，更新cache的同时确认；
+    //                       脏，那么等写完mem后，等到读mem时(isRM)，mem确认完数据(cache_data_data_ok)后，更新cache的同时确认；
+    // 如果是读指令load，命中，那么更新cache的同时确认数据；
+    //                 缺失，干净，那么等读mem时(isRM)，mem确认完数据(cache_data_data_ok)后，更新cache的同时确认；
+    //                      脏，那么等写完mem后，等到读mem时(isRM)，mem确认完数据(cache_data_data_ok)后，更新cache的同时确认；
+
+    // if (cpu_data_req & hit){
+    //     return 1'b1;
+    // }else if (isRM){
+    //     return cache_data_data_ok;
+    // }
+    // 即 -> cpu_data_req & hit | isRM & cache_data_data_ok;
+    assign cpu_data_data_ok = cpu_data_req & hit | isRM & cache_data_data_ok;
 
     //output to axi interface
-    assign cache_data_req   = read_req & ~addr_rcv | write_req & ~waddr_rcv;
-    assign cache_data_wr    = write_req;
+    // cache->mem
+    // 是不是数据请求(RM或WM状态)，且还没收到地址。一个周期后就清除了
+    assign cache_data_req   = isRM & ~addr_rcv | isWM & ~waddr_rcv;
+    // 当前请求是否是写请求。只要处在WM状态就是写请求，与load还是store指令无关。
+    assign cache_data_wr    = isWM;
+    // 确定数据的有效字节。
+    // 如果是load指令, 有效字节是4B（当前cpu_data_size）;
+    // 如果是store指令，根据sb, sh（当前cpu_data_size）确定不同的有效字节
     assign cache_data_size  = cpu_data_size;
-    // 如果要写内存，写回mem的地址为原cache line对应的地址（旧地址）
+    //* 如果要写内存，写回mem的地址为原cache line对应的地址（旧地址）
     // 如果是读内存，对应mem的地址为cpu_data_addr（新地址）
     assign cache_data_addr  = cache_data_wr ? {c_tag[c_way], index, offset}:
                                               cpu_data_addr;
-    // cache要写回memory的数据是原cache line的数据
+    //* cache要写回memory的数据是原cache line的数据
     assign cache_data_wdata = c_block[c_way];
 
     //写入Cache
@@ -216,12 +236,8 @@ module d_cache (
     //位拓展：{8{1'b1}} -> 8'b11111111
     //new_data = old_data & ~mask | write_data & mask
     wire [31:0] cache_tmp;
-    assign cache_tmp=   (~|(c_way^2'b00)) ?cache_block[index][1*DATA_WIDTH-1:0*DATA_WIDTH]:
-                        ( (~|(c_way^2'b01))? cache_block[index][2*DATA_WIDTH-1:1*DATA_WIDTH]:
-                            ((~|(c_way^2'b10))? cache_block[index][3*DATA_WIDTH-1:2*DATA_WIDTH]:
-                                                    cache_block[index][4*DATA_WIDTH-1:3*DATA_WIDTH]));
-
-
+    assign cache_tmp=   (~|(c_way^1'b0)) ?cache_block[index][1*DATA_WIDTH-1:0*DATA_WIDTH]:
+                                            cache_block[index][2*DATA_WIDTH-1:1*DATA_WIDTH];
     assign write_cache_data = cache_tmp & ~{{8{write_mask[3]}}, {8{write_mask[2]}}, {8{write_mask[1]}}, {8{write_mask[0]}}} | 
                               cpu_data_wdata & {{8{write_mask[3]}}, {8{write_mask[2]}}, {8{write_mask[1]}}, {8{write_mask[0]}}};
 
@@ -230,79 +246,51 @@ module d_cache (
     integer t, y;
     always @(posedge clk) begin
         if(rst) begin
-            for(t=0; t<CACHE_DEEPTH; t=t+1) begin   //刚开始将Cache初始化为无效，dirty 初始化为 0
-                for (y = 0; y<4; y=y+1) begin
+            for(t=0; t<CACHE_DEEPTH; t=t+1) begin   //刚开始将Cache初始化为无效，dirty 初始化为 0，//* ru 初始化为0
+                for (y = 0; y<2; y=y+1) begin
                     cache_valid[t][y] = 0;
                     cache_dirty[t][y] = 0;
+                    cache_ru   [t][y] = 0;
                 end
-                // tree初始化为000
-                tree_table[t] = 3'b000;
             end
         end
         else begin
             if(read_finish) begin // 处于RM状态，且已得到mem读取的数据
                 case(c_way)
-                    2'b00: begin
+                    1'b0: begin
                         cache_valid[index_save][0]<= 1'b1;  //将Cache line置为有效
                         cache_dirty[index_save][0] <= 1'b0;  // 读取内存的数据后，一定是clean
                         cache_tag  [index_save][1*TAG_WIDTH-1:0*TAG_WIDTH] <= tag_save;
                         cache_block[index_save][1*DATA_WIDTH-1:0*DATA_WIDTH] <= cache_data_rdata; //写入Cache line
                     end
-                    2'b01: begin
+                    1'b1: begin
                         cache_valid[index_save][1]<= 1'b1;  //将Cache line置为有效
                         cache_dirty[index_save][1] <= 1'b0;  // 读取内存的数据后，一定是clean
                         cache_tag  [index_save][2*TAG_WIDTH-1:1*TAG_WIDTH] <= tag_save;
                         cache_block[index_save][2*DATA_WIDTH-1:1*DATA_WIDTH] <= cache_data_rdata; //写入Cache line
                     end
-                    2'b10: begin
-                        cache_valid[index_save][2]<= 1'b1;  //将Cache line置为有效
-                        cache_dirty[index_save][2] <= 1'b0;  // 读取内存的数据后，一定是clean
-                        cache_tag  [index_save][3*TAG_WIDTH-1:2*TAG_WIDTH] <= tag_save;
-                        cache_block[index_save][3*DATA_WIDTH-1:2*DATA_WIDTH] <= cache_data_rdata; //写入Cache line
-                    end
-                    2'b11: begin
-                        cache_valid[index_save][3]<= 1'b1;  //将Cache line置为有效
-                        cache_dirty[index_save][3] <= 1'b0;  // 读取内存的数据后，一定是clean
-                        cache_tag  [index_save][4*TAG_WIDTH-1:3*TAG_WIDTH] <= tag_save;
-                        cache_block[index_save][4*DATA_WIDTH-1:3*DATA_WIDTH] <= cache_data_rdata; //写入Cache line
-                    end
                 endcase
             end
-            else if (write & isIDLE & (hit | in_RM)) begin 
-                // write指令，hit进入IDLE状态 或 从读内存回到IDLE后，将寄存器值的(部分)字节写入cache对应行
-                // 判断条件中加(hit | in_RM)是因为，如果只判断(write & isIDLE)，发生miss时，会在进入WM、RM之前提前进入该条件（本意是从RM回到IDLE的时候，已经读了mem的数据到cache后，再进入该条件，结果是刚进入write分支，就进入了该条件），
+            else if (store & isIDLE & (hit | in_RM)) begin 
+                // store指令，hit进入IDLE状态 或 从读内存回到IDLE后，将寄存器值的(部分)字节写入cache对应行
+                // 判断条件中加(hit | in_RM)是因为，如果只判断(store & isIDLE)，发生miss时，会在进入WM、RM之前提前进入该条件（本意是从RM回到IDLE的时候，已经读了mem的数据到cache后，再进入该条件，结果是刚进入store分支，就进入了该条件），
                 // 如果提前进入条件的话，此时写入cache的write_cache_data为 {旧cache[:x], 寄存器[x-1:0]}，WM时会把这个错误数据写回mem，导致出错。为解决该问题，额外加了一个信号in_RM，记录之前是不是一直处在RM状态。
                 case(c_way)
-                    2'b00: begin
+                    1'b0: begin
                         cache_dirty[index][0]                        <= 1'b1; // 改了数据，变dirty
                         cache_block[index][1*DATA_WIDTH-1:0*DATA_WIDTH]<= write_cache_data;  
                     end
-                    2'b01: begin
+                    1'b1: begin
                         cache_dirty[index][1]                        <= 1'b1; // 改了数据，变dirty
                         cache_block[index][2*DATA_WIDTH-1:1*DATA_WIDTH]<= write_cache_data;  
-                    end
-                    2'b10: begin
-                        cache_dirty[index][2]                        <= 1'b1; // 改了数据，变dirty
-                        cache_block[index][3*DATA_WIDTH-1:2*DATA_WIDTH]<= write_cache_data;  
-                    end
-                    2'b11: begin
-                        cache_dirty[index][3]                        <= 1'b1; // 改了数据，变dirty
-                        cache_block[index][4*DATA_WIDTH-1:3*DATA_WIDTH]<= write_cache_data;  
                     end
                 endcase
             end
 
-            if ((read | write) & isIDLE & (hit | in_RM)) begin
-                // read 或 write指令，hit进入IDLE状态 或 从读内存回到IDLE后，更新伪LRU表
-                // 分析：
-                // c_way == 2'b00, 使用了way0，更新右子树，{tree[2], [1]} 更新为 2'b11;
-                // c_way == 2'b01, 使用了way1，更新右子树，{tree[2], [1]} 更新为 2'b10;
-                // c_way == 2'b10, 使用了way2，更新左子树，{tree[2], [0]} 更新为 2'b01;
-                // c_way == 2'b11, 使用了way3，更新左子树，{tree[2], [0]} 更新为 2'b00;
-                if (c_way[1] == 1'b0)
-                    {tree_table[index][2], tree_table[index][1]} <= ~c_way;
-                else
-                    {tree_table[index][2], tree_table[index][0]} <= ~c_way;
+            if ((load | store) & isIDLE & (hit | in_RM)) begin
+                //* load 或 store指令，hit进入IDLE状态 或 从读内存回到IDLE后，将最近使用情况更新
+                cache_ru[index][c_way]   <= 1'b1; //* c_way 路最近使用了
+                cache_ru[index][1-c_way] <= 1'b0; //* 1-c_way 路最近未使用
             end
         end
     end
