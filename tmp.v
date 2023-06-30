d20c1c63690adccddd1e6996513c30d2e4048113
kachi3 <971759126@qq.com>
2023-06-30 10:20:40 +0800

    deleted:    tmp.v

--- tmp.v
+++
@@ -1,6 +0,0 @@
-   wire [31:0] pcplus4F;    //pc
-    wire [31:0] instrF_4;                   //instrF末尾为2'b00
-    
-    wire pc_errorF;  // pc错误
-
-    wire is_in_delayslot_iF; // 此时的D阶段（即上一条指令）是否为跳转指令
