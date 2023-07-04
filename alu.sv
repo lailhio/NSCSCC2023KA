2cfc666458531a4955726333ec50a450007d6d0a
kachi <971759126@qq.com>
2023-07-05 01:34:26 +0800

    完成对alu电路的缩减

--- alu.sv
+++ alu.sv
@@ -9,81 +9,91 @@ module alu(
     input wire [4:0] alucontrolE, 
     input wire [4:0] sa, 
     
-    output wire hilo_writeE,
-    output wire [1:0]hilo_selectE,
+    output reg hilo_writeE,
+    output reg [1:0]hilo_selectE,
     output wire alustallE,
-    output wire [63:0] aluoutE, 
-    output wire overflowE
+    output reg [63:0] aluoutE, 
+    output reg overflowE
 );
     wire [63:0] aluout_div; 
     wire [63:0] aluout_mul;
-    wire mul_sign;
-    wire mul_valid;  
-    wire mul_stallE;
-    wire div_sign; 
-	wire div_valid; 
-    wire div_stallE;
+    reg mul_sign;
+    reg div_sign; 
 	wire ready_div;
     wire ready_mul;
-    reg [31:0] aluout_simple; 
-    reg carry_bit; 
+    wire mul_stallE;
+    wire div_stallE;
 
-    
-    //aluout
-    assign aluoutE = ({64{div_valid}} & aluout_div)
-                    | ({64{mul_valid}} & aluout_mul)
-                    | ({64{~mul_valid & ~div_valid}} & {32'b0, aluout_simple})
-                    | ({64{~|(alucontrolE ^ `MTHI_CONTROL)}} & {src_aE, 32'b0})
-                    | ({64{~|(alucontrolE ^ `MTLO_CONTROL)}} & {32'b0, src_aE});
+    assign mul_stallE = (~|(alucontrolE[4:1]^4'b1100) & ~ready_mul);
+    assign div_stallE = (~|(alucontrolE[4:1]^4'b1101) & ~ready_div);
+    assign alustallE=  mul_stallE | div_stallE;
 
-    assign overflowE = ((~|(alucontrolE^`ADD_CONTROL)) | (~|(alucontrolE^`SUB_CONTROL))) & 
-                        (carry_bit ^ aluout_simple[31]);
-
+    assign hilo_writeE  =  ready_div | ready_mul |
+                        ((~|(alucontrolE[4:1]^ 4'b1100)) | 
+                        ((~|({alucontrolE[4:2],alucontrolE[0]}^ 4'b1111)) & ~stallE));
    
     always @(*) begin
-        carry_bit = 0;
+        hilo_selectE = 2'b00;
+        overflowE = 1'b0;
         case(alucontrolE)
-            `AND_CONTROL:       aluout_simple = src_aE & src_bE;
-            `OR_CONTROL:        aluout_simple = src_aE | src_bE;
-            `NOR_CONTROL:       aluout_simple =~(src_aE | src_bE);
-            `XOR_CONTROL:       aluout_simple = src_aE ^ src_bE;
+            `AND_CONTROL:       aluoutE = src_aE & src_bE;
+            `OR_CONTROL:        aluoutE = src_aE | src_bE;
+            `NOR_CONTROL:       aluoutE =~(src_aE | src_bE);
+            `XOR_CONTROL:       aluoutE = src_aE ^ src_bE;
 
-            `ADD_CONTROL:       {carry_bit, aluout_simple} = {src_aE[31], src_aE} + {src_bE[31], src_bE};
-            `ADDU_CONTROL:      aluout_simple = src_aE + src_bE;
-            `SUB_CONTROL:       {carry_bit, aluout_simple} = {src_aE[31], src_aE} - {src_bE[31], src_bE};
-            `SUBU_CONTROL:      aluout_simple = src_aE - src_bE;
+            `ADD_CONTROL:begin
+                aluoutE = {src_aE[31], src_aE} + {src_bE[31], src_bE};
+                overflowE= (src_aE[31] == src_bE[31]) & (aluoutE[31] != src_aE[31]);
+            end
+            `ADDU_CONTROL:      aluoutE = src_aE + src_bE;
+            `SUB_CONTROL:begin
+                aluoutE= {src_aE[31], src_aE} - {src_bE[31], src_bE};
+                overflowE =(src_aE[31]^src_bE[31]) & (aluoutE[31]==src_bE[31]);;
+            end
+            `SUBU_CONTROL:      aluoutE = src_aE - src_bE;
 
-            `SLT_CONTROL:       aluout_simple = $signed(src_aE) < $signed(src_bE); 
-            `SLTU_CONTROL:      aluout_simple = src_aE < src_bE; 
+            `SLT_CONTROL:       aluoutE = $signed(src_aE) < $signed(src_bE); 
+            `SLTU_CONTROL:      aluoutE = src_aE < src_bE; 
+            //Mov Cmd
+            `SLLV_CONTROL:       aluoutE = src_bE << src_aE[4:0]; 
+            `SRLV_CONTROL:       aluoutE = src_bE >> src_aE[4:0];
+            `SRAV_CONTROL:       aluoutE = $signed(src_bE) >>> src_aE[4:0];
 
-            `SLLV_CONTROL:       aluout_simple = src_bE << src_aE[4:0]; 
-            `SRLV_CONTROL:       aluout_simple = src_bE >> src_aE[4:0];
-            `SRAV_CONTROL:       aluout_simple = $signed(src_bE) >>> src_aE[4:0];
+            `SLL_CONTROL:    aluoutE = src_bE << sa; 
+            `SRL_CONTROL:    aluoutE = src_bE >> sa;
+            `SRA_CONTROL:    aluoutE = $signed(src_bE) >>> sa;
 
-            `SLL_CONTROL:    aluout_simple = src_bE << sa; 
-            `SRL_CONTROL:    aluout_simple = src_bE >> sa;
-            `SRA_CONTROL:    aluout_simple = $signed(src_bE) >>> sa;
+            `LUI_CONTROL:       aluoutE = {src_bE[15:0], 16'b0};
+            `MULT_CONTROL  : begin
+                mul_sign = 1'b1;
+                if(ready_mul) aluoutE = aluout_mul;
+            end
+            `MULTU_CONTROL  : begin
+                mul_sign = 1'b0;
+                if(ready_mul) aluoutE = aluout_mul;
+            end
+            `DIV_CONTROL :begin
+                div_sign = 1'b1;
+                if(ready_div) aluoutE = aluout_div;
+            end
+            `DIVU_CONTROL :begin
+                div_sign = 1'b0;
+                if(ready_div) aluoutE = aluout_div;
+            end
+            `MTHI_CONTROL: begin
+                aluoutE = {src_aE, 32'b0};
+                hilo_selectE = 2'b11;
+            end
+            `MTLO_CONTROL: begin
+                aluoutE = {32'b0, src_aE};
+                hilo_selectE = 2'b10;
+            end
+            5'b00000: aluoutE = src_aE;  // do nothing
 
-            `LUI_CONTROL:       aluout_simple = {src_bE[15:0], 16'b0};
-            5'b00000: aluout_simple = src_aE;  // do nothing
-
-            default:    aluout_simple = 32'b0;
+            default:    aluoutE = 32'b0;
         endcase
     end
-    assign hilo_selectE={(~|(alucontrolE[4:2] ^ 3'b111)),(~|(alucontrolE ^ `MTHI_CONTROL))};//高位1表示是mhl指令，0表示是乘除法
-                                                                                            //低位1表示是用hi，0表示用lo
-    assign hilo_writeE  =  ready_div | ready_mul |
-                        // ((~|(alucontrolE[4:1]^ 4'b1100)) | 
-                        ((~|({alucontrolE[4:2],alucontrolE[0]}^ 4'b1111)) & ~stallE);
 
-    assign mul_sign = ~|(alucontrolE ^ `MULT_CONTROL);
-    assign mul_valid = ~|(alucontrolE[4:1]^4'b1100);
-    assign mul_stallE= ready_mul ? 0 : mul_valid; 
-    
-    assign div_sign = ~|(alucontrolE ^ `DIV_CONTROL);
-    assign div_valid = ~|(alucontrolE[4:1] ^ 4'b1101);
-    assign div_stallE= ready_div ? 0 : div_valid; 
-    assign alustallE = mul_stallE | div_stallE;
 
     mul mul(
 		.clk(~clk),
