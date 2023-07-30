6aada412b4e0524f30749c5cb867eb9fb6ea28cf
chuxiaodong <2549369356@qq.com>
2023-07-30 16:52:07 +0800

    添加ascii

--- instdec.sv
+++ instdec.sv
@@ -1,131 +1,140 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2018/08/06 15:21:16
-// Design Name: 
-// Module Name: instdec
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-`timescale 1ns / 1ps
-
 `include "defines2.vh"
-
 
 module instdec(
     input wire [31:0] instr,
-    output reg [39:0] ascii
+    output reg [127:0] ascii
     );
 
-    always @(*)
-    begin
+    //Instruct Divide
+	wire [5:0] opD,functD;
+	wire [4:0] rsD,rtD,rdD,shamtD;
+	assign opD = instr[31:26];
+	assign functD = instr[5:0];
+	assign rsD = instr[25:21];
+	assign rtD = instr[20:16];
+	assign rdD = instr[15:11];
+	assign shamtD = instr[10:6];
+
+    //ALU.sv sort
+    always @(*)begin
         ascii ="N-R";
-        case(instr[31:26])
-            `NOP:   // R-type
-                begin
-                    case(instr[5:0])
-                        /* logic instraction */
-                        `AND: ascii = "AND";
-                        `OR: ascii = "OR";
-                        `XOR: ascii = "XOR";
-                        `NOR: ascii = "NOR";
-                        /* shift instraction */
-                        `SLL: ascii = "SLL";
-                        `SRL: ascii = "SRL";
-                        `SRA: ascii = "SRA";
-                        `SLLV: ascii = "SLLV";
-                        `SRLV: ascii = "SRLV";
-                        `SRAV: ascii = "SRAV";
-                        /* move instraction */
-                        `MFHI: ascii = "MFHI";
-                        `MTHI: ascii = "MTHI";
-                        `MFLO: ascii = "MFLO";
-                        `MTLO: ascii = "MTLO";
-                        /* arithemtic instraction */
-                        `ADD: ascii = "ADD";
-                        `ADDU: ascii = "ADDU";
-                        `SUB: ascii = "SUB";
-                        `SUBU: ascii = "SUBU";
-                        `SLT: ascii = "SLT";
-                        `SLTU: ascii = "SLTU";
-
-                        `MULT: ascii = "MULT";
-                        `MULTU: ascii = "MULTU";
-                        `DIV: ascii = "DIV";
-                        `DIVU: ascii = "DIVU";
-                        /* jump instraction */
-                        `JR: ascii = "JR";
-                        `JALR: ascii = "JALR";
-                        
-                        `SYSCALL: ascii = "SYSC";
-                        `BREAK: ascii = "BRE";
-                        default: ascii ="N-R";
-                    endcase
-                end
-            `ANDI: ascii = "ANDI";
-            `XORI: ascii = "XORI";
-            `LUI: ascii = "LUI";
-            `ORI: ascii = "ORI";
-
+        case(opD)
+            //R
+            `NOP:begin
+                case(functD)
+                    `ADD: ascii = "ADD";
+                    `ADDU: ascii = "ADDU";
+                    `SUB: ascii = "SUB";
+                    `SUBU: ascii = "SUBU";
+                    `SLTU: ascii = "SLTU";
+                    `SLT: ascii = "SLT";
+                    `AND: ascii = "AND";
+                    `NOR: ascii = "NOR";
+                    `OR: ascii = "OR";
+                    `XOR: ascii = "XOR";
+                    `SLLV: ascii = "SLLV";
+                    `SLL: ascii = "SLL";
+                    `SRAV: ascii = "SRAV";
+                    `SRA: ascii = "SRA";
+                    `MOVN: ascii = "MOVN";
+                    `MOVZ: ascii = "MOVZ";
+                    `MFHI: ascii = "MFHI";
+                    `MFLO: ascii = "MFLO";
+                    `SRL: ascii = "SRL";
+                    `SRLV: ascii = "SRLV";
+                    `JR: ascii = "JR";
+                    `MULT: ascii = "MULT";
+                    `MULTU: ascii = "MULTU";
+                    `DIV: ascii = "DIV";
+                    `DIVU: ascii = "DIVU";
+                    `MTHI: ascii = "MTHI";
+                    `MTLO: ascii = "MTLO";
+                    `JALR: ascii = "JALR";
+                    `SYSCALL: ascii = "SYSCALL";
+                    `BREAK: ascii = "BREAK";
+                    default: ascii ="N-R";
+                endcase
+            end
+            // 算数\逻辑运算
             `ADDI: ascii = "ADDI";
-            `ADDIU: ascii = "ADDIU";
             `SLTI: ascii = "SLTI";
             `SLTIU: ascii = "SLTIU";
-
-            `J: ascii = "J";
-            `JAL: ascii = "JAL";
-            
+            `ADDIU: ascii = "ADDIU";
+            `ANDI: ascii = "ANDI";
+            `LUI: ascii = "LUI";
+            `XORI: ascii = "XORI";
+            `ORI: ascii = "ORI";
             `BEQ: ascii = "BEQ";
             `BGTZ: ascii = "BGTZ";
             `BLEZ: ascii = "BLEZ";
             `BNE: ascii = "BNE";
-            
+            `REGIMM_INST: begin
+				case(rtD)
+                    `BLTZ: ascii = "BLTZ";
+                    `BGEZ: ascii = "BGEZ";
+                    `BLTZAL: ascii = "BLTZAL";
+                    `BGEZAL: ascii = "BGEZAL";
+                    default: ascii = "N-R";
+                endcase
+            end
+            // 访存指令，都是立即数指令
+            `LW: ascii = "LW";
             `LB: ascii = "LB";
             `LBU: ascii = "LBU";
             `LH: ascii = "LH";
             `LHU: ascii = "LHU";
-            `LW: ascii = "LW";
+            `LWL: ascii = "LWL";
+            `LWR: ascii = "LWR";
+            `LL: ascii = "LL";
+            `SW: ascii = "SW";
             `SB: ascii = "SB";
             `SH: ascii = "SH";
-            `SW: ascii = "SW";
-            6'b000001: begin 
-                case (instr[20:16])
-                    `BGEZ: ascii= "BGEZ";
-                    `BGEZAL: ascii= "BGEZAL";
-                    `BLTZ: ascii= "BLTZ";
-                    `BLTZAL: ascii= "BLTZAL";
-                    default : ascii= " ";
+            `SWL: ascii = "SWL";
+            `SWR: ascii = "SWR";
+            //  J type
+            `J: ascii = "J";
+            `JAL: ascii = "JAL";
+            
+            `COP0_INST:begin
+				case(rsD)
+                    `MFC0: ascii = "MFC0";
+                    `MTC0: ascii = "MTC0";
+                    default: ascii = "N-R";
                 endcase
-            end
-            6'b010000: begin 
-                if(instr==`ERET) begin
-                    ascii ="ERET";
-                end else begin 
-                    case (instr[25:21])
-                        5'b00100: ascii="MTOC0";
-                        5'b00000: ascii="MFC0";
-                    endcase
-                end
-            end
+			end
+            `SPECIAL2_INST: begin
+				case(functD)
+                    `CLO: ascii = "CLO";
+                    `CLZ: ascii = "CLZ";
+                    `MUL: ascii = "MUL";
+                    `MADD: ascii = "MADD";
+                    `MADDU: ascii = "MADDU";
+                    `MSUB: ascii = "MSUB";
+                    `MSUBU: ascii = "MSUBU";
+                    default: ascii = "N-R";
+                endcase
+			end
+            `SPECIAL3_INST: begin
+				case(functD)
+                    `BSHFL: begin
+                        case(shamtD)
+                            `SEB: ascii = "SEB";
+                            `SEH: ascii = "SEH";
+                            `WSBH: ascii = "WSBH";
+                            default: ascii = "N-R";
+                        endcase
+                    end
+                    `EXT: ascii = "EXT";
+                    `INS: ascii = "INS";
+                    default: ascii = "N-R";
+                endcase
+			end
             default: ascii = "N-R";
-       endcase
-    if(instr==`ERET)
-        ascii = "ERET";
-    if(!instr)
-        ascii = "NOP";
+        endcase
+        if(instr==`ERET)
+            ascii = "ERET";
+        if(!instr)
+            ascii = "NOP";
     end
-
 endmodule
 
