f9309532c039f5a38e042f4055119b18a0973e17
kachi <971759126@qq.com>
2023-08-03 21:13:18 +0800

    增加暂停优先的流水器

---
+++ flopsf.sv
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//flip-flop with enable,rst,clear
+module flopsf #(parameter WIDTH = 8)(
+	input wire clk,rst,stall,flush,
+	input wire[WIDTH-1:0] in,
+	output reg[WIDTH-1:0] out
+    );
+	always @(posedge clk) begin
+		if(rst) begin
+			out <= 0;
+		end else if(~stall) begin
+			/* code */
+			out <= in;
+		end
+        else if(flush)begin
+            out <= 0;
+        end
+        else begin
+            out <= out;
+        end
+	end
+endmodule
