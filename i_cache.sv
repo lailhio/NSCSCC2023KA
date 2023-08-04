40cadfcadf0de6fcde030a65df2b3fb563e81310
kachi3 <971759126@qq.com>
2023-08-04 23:00:23 +0800

    修改cache逻辑

--- i_cache.sv
+++ i_cache.sv
@@ -1,7 +1,6 @@
 module i_cache #(
-    parameter LEN_LINE = 6,  // 64 Bytes
+    parameter LEN_LINE = 6,  // 32 Bytes
     parameter LEN_INDEX = 7, // 128 lines
-    parameter LEN_DATA = 3,
     parameter NR_WAYS = 2
 ) (
     input wire clk, rst, no_cache, icache_Ctl,
@@ -10,8 +9,7 @@ module i_cache #(
     input  [31:0] cpu_inst_addr    ,
     input         cpu_inst_en      ,
     //mips core output
-    output [31:0] cpu_inst1_rdata   ,
-    output [31:0] cpu_inst2_rdata   ,
+    output [31:0] cpu_inst_rdata   ,
     //I CACHE
     output reg [31:0] i_araddr,
     output reg [7:0] i_arlen,
@@ -30,7 +28,6 @@ module i_cache #(
     localparam LEN_BRAM_ADDR = LEN_LINE - 3 + LEN_INDEX;
     localparam CACHE_DEEPTH = 1 << LEN_INDEX;
     localparam NR_WORDS = 1 << (LEN_LINE - 2);
-    localparam DATA_WIDTH = 1 << (LEN_DATA + 3);
 
     
     (*ram_style="block"*) reg [1:0]               cache_valid [CACHE_DEEPTH - 1 : 0];
@@ -50,11 +47,12 @@ module i_cache #(
     reg [LEN_LINE-1:0] lineLoc_IF3;
     reg [LEN_INDEX-1:0] index_IF3;
     reg [LEN_TAG-1:0] tag_IF3;
+    reg  no_cache_IF3;
     reg tway_IF3;
     reg  cpu_inst_en_IF3;
     
     logic [1:0] wena_tag_ram_way;
-    logic [7:0] wena_data_bank_way [NR_WAYS-1:0];
+    logic [3:0] wena_data_bank_way [NR_WAYS-1:0];
     
     assign lineLoc = cpu_inst_addr[LEN_LINE - 1 : 0];
     assign index = cpu_inst_addr[LEN_INDEX + LEN_LINE - 1 : LEN_LINE];
@@ -65,8 +63,8 @@ module i_cache #(
     //  IF2
     reg  [1:0]           c_valid_IF2;
     reg  [1:0]           c_lru_IF2; //* recently used
-    reg [LEN_TAG-1:0]    c_tag_IF2  [1:0];
-    reg [DATA_WIDTH-1:0] c_block_IF2[1:0];
+    reg [LEN_TAG-1:0]   c_tag_IF2  [1:0];
+    reg [31:0]          c_block_IF2[1:0];
     wire [1:0]           c_way;
 	
     reg[1:0]            c_valid_IF3;
@@ -88,27 +86,20 @@ module i_cache #(
     assign inst_en = isIDLE ? cpu_inst_en_IF2 : cpu_inst_en_IF3;
 
     // hit Control
-    assign hit = |c_way & isIDLE;  //* cache line
+    assign hit = |c_way & isIDLE & ~no_cache_IF2;  //* cache line
     assign miss = ~hit;
 
-    assign c_way[0] = c_valid_IF2[0] & c_tag_IF2[0] == tag_IF2;
-    assign c_way[1] = c_valid_IF2[1] & c_tag_IF2[1] == tag_IF2;
-    wire   cache_hit_available = hit  & !no_cache_IF2;
-    assign i_stall = (~isIDLE | (!cache_hit_available & inst_en)) & ~cpu_instr_ok;
-    wire cache_en = ~icache_Ctl | i_stall; 
+    assign c_way[0] = c_valid_IF2[0] & ((c_tag_IF2[0] == tag_IF2));
+    assign c_way[1] = c_valid_IF2[1] & ((c_tag_IF2[1] == tag_IF2));
+
+    assign i_stall = ~hit  & ~cpu_instr_ok & inst_en;
+
+    wire cache_en1 =  ~(i_stall | icache_Ctl);
+    wire cache_en = ~cpu_instr_ok & inst_en; 
     //output to mips core
     // first stage is not stall, and the second judge whether to stall
-    reg  inst2_Ren;
-    reg [31:0] axi_inst1_rdata;
-    reg [31:0] axi_inst2_rdata;
-    wire [31:0] cache_inst1_rdata;
-    wire [31:0] cache_inst2_rdata;
-
-    assign cache_inst1_rdata = lineLoc_IF2[2] ? 32'b0: c_block_IF2[c_way[1]][31:0];
-    assign cache_inst2_rdata = c_block_IF2[c_way[1]][63:32];
-    
-    assign cpu_inst1_rdata   = pre_state != IDLE ? axi_inst1_rdata : cache_inst1_rdata;
-    assign cpu_inst2_rdata   = pre_state != IDLE ? axi_inst2_rdata : cache_inst2_rdata;
+    reg [31:0] axi_inst_rdata;
+    assign cpu_inst_rdata   = pre_state != IDLE ? axi_inst_rdata : c_block_IF2[c_way[1]];
 
     // axi cnt
     logic [LEN_LINE-1:2] axi_cnt;
@@ -125,7 +116,7 @@ module i_cache #(
             c_valid_IF2 <= 0;
             c_lru_IF2 <= 0;
         end
-        else if(cache_en)begin
+        else if(cache_en1)begin
             lineLoc_IF2 <= lineLoc;
             index_IF2 <= index;
             tag_IF2 <= tag;
@@ -145,9 +136,7 @@ module i_cache #(
             index_IF3 <= 0;
             lineLoc_IF3 <= 0;
             tag_IF3 <= 0;
-            inst2_Ren <= 0;
-            axi_inst1_rdata <= 0;
-            axi_inst2_rdata <= 0;
+            axi_inst_rdata <= 0;
             // clear status
             wena_data_bank_way <= '{default: '0};
             wena_tag_ram_way <= '{default: '0};
@@ -166,11 +155,10 @@ module i_cache #(
             // clear axi status
             axi_cnt <= 0;
         end
-        else if (inst_en & cache_en)begin
+        else if (cache_en)begin
             pre_state <= state;
             case(state)
                 IDLE: begin
-                    cpu_instr_ok <= 1'b0;
                     cpu_inst_en_IF3 <= cpu_inst_en_IF2;
                     index_IF3 <= index_IF2;
                     lineLoc_IF3 <= lineLoc_IF2;
@@ -192,18 +180,14 @@ module i_cache #(
                         i_arlen <= NR_WORDS - 1;
                         i_arsize <= 3'd2; //4 bytes
                         i_arvalid <= 1'b1;  //read addr is valid
-                        //initial
-                        axi_inst1_rdata <= 32'b0;
-                        axi_inst2_rdata <= 32'b0;
                         // Write ena
-                        wena_data_bank_way[c_lru_IF2[1]] <= 8'h0f;// write to instram
-                        wena_data_bank_way[~c_lru_IF2[1]] <= 8'h00;
+                        wena_data_bank_way[c_lru_IF2[1]] <= 4'hf;// write to instram
+                        wena_data_bank_way[~c_lru_IF2[1]] <= 4'h0;
                         wena_tag_ram_way <= {c_lru_IF2[1],~c_lru_IF2[1]}; //write to tag
                         cache_valid[index_IF2][c_lru_IF2[1]] <= 1'b1;
                         cache_lru[index_IF2][c_lru_IF2[1]] <=1'b0;
                         cache_lru[index_IF2][~c_lru_IF2[1]] <=1'b1;
                         axi_cnt <= 0;
-                        inst2_Ren <= 0;
                         state <= CACHE_REPLACE;
                     end
                     else begin
@@ -222,8 +206,7 @@ module i_cache #(
                     end
                     else if (i_rvalid & i_rready) begin
                         i_rready <= 1'b0;
-                        axi_inst1_rdata <= lineLoc_IF3[2] ? 32'b0 : i_rdata;
-                        axi_inst2_rdata <= lineLoc_IF3[2] ? i_rdata : 32'b0;
+                        axi_inst_rdata <= i_rdata;
                     end
                     else if (~i_rvalid & ~i_rready)begin
                         cpu_instr_ok <=1;
@@ -241,23 +224,15 @@ module i_cache #(
                         if (i_rvalid & i_rready) begin
                             if (!i_rlast) begin
                                 axi_cnt <= axi_cnt + 1;
-                                if(axi_cnt[2])
-                                    wena_data_bank_way[tway_IF3] <= 8'h0f;
-                                else
-                                    wena_data_bank_way[tway_IF3] <= 8'hf0;
+                                if(axi_cnt == lineLoc_IF3[LEN_LINE-1:2]) 
+                                    axi_inst_rdata <= i_rdata;
                             end
                             else begin
                                 i_rready <= 0;
                                 wena_data_bank_way[tway_IF3] <= 0;
                                 wena_tag_ram_way[tway_IF3] <= 0;
-                            end
-                            if(axi_cnt[LEN_LINE-1:3] == lineLoc_IF3[LEN_LINE-1:3])  begin
-                                inst2_Ren <= 1;
-                                axi_inst1_rdata <= lineLoc_IF3[2] ? 32'b0 : i_rdata;
-                            end
-                            else if(inst2_Ren)begin
-                                inst2_Ren <= 0;
-                                axi_inst2_rdata <= i_rdata;
+                                if(axi_cnt == lineLoc_IF3[LEN_LINE-1:2]) 
+                                    axi_inst_rdata <= i_rdata;
                             end
                         end
                         else if (!i_rready) begin // wait the final data write to bram.
@@ -273,9 +248,10 @@ module i_cache #(
                 // end
             endcase
         end
-        else if(cache_en)begin
+        else if(cache_en1)begin
             pre_state <= state;
             cpu_instr_ok <= 0;
+            no_cache_IF3 <= no_cache_IF2;
         end
     end
 
@@ -287,24 +263,24 @@ module i_cache #(
             (
             .clka   (clk),
             .clkb   (clk),
-            .ena    (wena_tag_ram_way[i]),
-            .enb    (cache_en),
+            .ena    (1'b1),
+            .enb    (cache_en1),
             .addra  (index_IF3),
             .dina   (tag_IF3),
             .wea    (wena_tag_ram_way[i]),
             .addrb  (index),
             .doutb  (c_tag_IF2[i])
             );
-            cache_block_ram #(.LEN_DATA(DATA_WIDTH),.LEN_ADDR(LEN_PER_WAY-3)) i_data
+            cache_block_ram #(.LEN_DATA(32),.LEN_ADDR(LEN_PER_WAY-2)) i_data
             (
             .clka   (clk),
             .clkb   (clk),
             .ena    (1'b1),
-            .enb    (cache_en),
-            .addra  ({index_IF3,axi_cnt[LEN_LINE-1:3]}),
-            .dina   ({i_rdata, i_rdata}),
+            .enb    (cache_en1),
+            .addra  ({index_IF3,axi_cnt[LEN_LINE-1:2]}),
+            .dina   (i_rdata),
             .wea    (wena_data_bank_way[i]),
-            .addrb  ({index,lineLoc[LEN_LINE-1:3]}),
+            .addrb  ({index,lineLoc[LEN_LINE-1:2]}),
             .doutb  (c_block_IF2[i])
             );
         end
