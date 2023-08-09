bbdf0ad4ae56bad13ef90942986ecda31412360b
kachi <971759126@qq.com>
2023-08-09 15:24:48 +0800

    修改flopctrl

--- flopctrl.sv
+++ flopctrl.sv
@@ -7,7 +7,24 @@ module flopctrl(
     );
 	always @(posedge clk) begin
 		if(rst | flush) begin
-			out <= '{default:'0};
+			out.sign_ex <= 0;
+			out.regdst <= 0;
+			out.is_imm <= 0;
+			out.regwrite <= 0;
+			out.read_rs <= 0;
+			out.read_rt <= 0;
+			out.mem_read <= 0;
+			out.mem_write <= 0;
+			out.memtoreg <= 0;
+			out.hilo_write <= 0;
+			out.hilo_read <= 0;
+			out.ri <= 0;
+			out.breaks <= 0;
+			out.syscall <= 0;
+			out.eret <= 0;
+			out.cp0_write <= 0;
+			out.cp0_read <= 0;
+			out.DivMulEn <= 0;
 		end else if(~stall) begin
 			out <= in;
 		end
